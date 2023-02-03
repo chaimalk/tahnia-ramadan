e180189a1f1ca2491bd3c27dd3c5d50a1f1bd70b
chaimalk <108950943+chaimalk@users.noreply.github.com>
2023-02-03 02:06:39 +0100

    Create ramadan.ads

---
+++ ramadan.ads
@@ -0,0 +1,13 @@
+{
+  "AdsController": {
+    "adNetwork": "unity",
+    "admobBanner": "ca-app-pub-3940256099942544/6300978111",
+    "admobInterstitial": "ca-app-pub-3940256099942544/1033173712",
+    "applovinBanner": "2a5604d7dcf11aa7",
+    "applovinInterstitial": "b12e2c2d46a3d129",
+    "unityGameId": "5147234",
+    "testMode": true,
+    "unityBanner": "Banner_Android",
+    "unityInterstitial": "Interstitial_Android"
+  }
+}
