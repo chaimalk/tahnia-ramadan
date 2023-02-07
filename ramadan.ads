2822c9a5711784eb07e696776f7d9930538b4c76
chaimalk <108950943+chaimalk@users.noreply.github.com>
2023-02-07 19:44:49 +0100

    Update ramadan.ads

--- ramadan.ads
+++ ramadan.ads
@@ -1,8 +1,8 @@
 {
   "AdsController": {
     "adNetwork": "admob",
-    "admobBanner": "ca-app-pub-1010770342437944/9046839095",
-    "admobInterstitial": "ca-app-pub-1010770342437944/3257108756",
+    "admobBanner": "ca-app-pub-9988228079010380/8766264345",
+    "admobInterstitial": "ca-app-pub-9988228079010380/1187157228",
     "applovinBanner": "2a5604d7dcf11aa7",
     "applovinInterstitial": "b12e2c2d46a3d129",
     "unityGameId": "5147234",
